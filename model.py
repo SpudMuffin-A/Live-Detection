e58d740a886c97bc8e8b1befc7cbbca26f836a44
SpudMuffin-A <55594646+SpudMuffin-A@users.noreply.github.com>
2021-10-04 19:51:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,10 +2,16 @@ from pydantic import BaseModel
 
 class Image(BaseModel):
     filename: str
-    img_dimensions: str
+    # img_dimensions: str
     encoded_img: str
+
+class ImageResponse(BaseModel):
+    filename: str
+    # img_dimensions: str
+    encoded_img: bytes
 
 class Todo(BaseModel):
     title: str
     description: str
+    
 
